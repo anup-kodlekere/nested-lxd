949686b6fc3c9b2173fdb74a1673479e4a623db6
Anup Kodlekere <100858233+anup-kodlekere@users.noreply.github.com>
2024-01-19 11:12:14 +0530

    [skip ci] Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-lxc launch ubuntu:22.04 test
+if [[ "${CI}" = true ]]; then
+  PREFIX="travis_wait 20"
+  LXD_FLAGS="-c security.privileged=true"
+  SU="sudo"
+else
+  PREFIX=""
+  LXD_FLAGS=""
+  SU=""
+fi
+
+
+${PREFIX} ${SU} lxc launch ubuntu:22.04 test ${LXD_FLAGS}
 lxc ls
